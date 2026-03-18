a84f34ce3ef078ebf649d849f60c929f28fbff64
genesisTerraria <abca41833@gmail.com>
2026-03-18 01:58:19 -0400

    changed world spawnpoint and imported lobby

--- Content/ServerSide/GameManager.cs
+++ Content/ServerSide/GameManager.cs
@@ -127,8 +127,8 @@ public class GameManager : ModSystem
     public override void OnWorldLoad()
     {
         // TODO: Re-Paste the Arena on world load (in case it gets destroyed by an admin).
-        Main.spawnTileX = 8976 / 16; //spawn coords for the world ONLY on world load (this is changed later)
-        Main.spawnTileY = 11120 / 16;
+        Main.spawnTileX = 11152 / 16; //spawn coords for the world ONLY on world load (this is changed later)
+        Main.spawnTileY = 4128 / 16;
 
         BlueGem = new Gem(new Vector2(CTG2.config.BlueGem[0], CTG2.config.BlueGem[1]), 3);
         RedGem = new Gem(new Vector2(CTG2.config.RedGem[0], CTG2.config.RedGem[1]), 1);
@@ -474,8 +474,8 @@ public class GameManager : ModSystem
             ModPacket teleportPacket = mod.GetPacket();
             teleportPacket.Write((byte)MessageType.ServerTeleport);
             teleportPacket.Write(player.whoAmI);
-            teleportPacket.Write((int)8976);
-            teleportPacket.Write((int)11120);
+            teleportPacket.Write((int)11152);
+            teleportPacket.Write((int)4128);
             teleportPacket.Send(toClient: player.whoAmI);
 
             // Clear inventory and reset player stats
