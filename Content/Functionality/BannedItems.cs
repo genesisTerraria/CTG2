2eabdbd00ea2ff891d02e3f636d9eb7199996599
Zialith06 <ajkington969@gmail.com>
2026-05-11 00:50:05 +0100

    updated banned item list

--- Content/Functionality/BannedItems.cs
+++ Content/Functionality/BannedItems.cs
@@ -13,6 +13,20 @@ namespace CTG2.Content.Functionality
         {
             ItemID.WoodenHammer,
             4909, // StickyDirtBomb
+            4824, // Wet Bomb
+            4825, // Honey Bomb
+            4826, // Lava Bomb
+            205, // Empty Bucket
+            206, // Water Bucket
+            207, // Lava Bucket
+            1128, // Honey Bucket
+            3031, // Bottomless Water Bucket
+            4820, // Bottomless Lava Bucket
+            5302, // Bottomless Honey Bucket
+            5364, // Bottomless Shimmer Bucket
+            4447, // Wet Rocket
+            4448, // Lava Rocket
+            4449, // Honey Rocket
             71, // Copper coin
             72, // Silver coin
             73, // Gold coin
