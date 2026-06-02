cd6865a13aec235b5896dfcaec52fc2f489004fd
denniscodes123 <dadennis2005@gmail.com>
2026-06-02 01:35:51 -0400

    Add 'UnCalamityModMusic' to allowed mods list

--- CTG2.cs
+++ CTG2.cs
@@ -2358,7 +2358,7 @@ namespace CTG2
                     string clientModsRaw = reader.ReadString();
                     string[] clientMods = clientModsRaw.Split(',');
 
-                    string[] allowedMods = { "CTG2", "OptimizedCursor", "HighFPSSupport", "TeamSpectate", "ModLoader", "Terraria", "PlayerBars", "Reese" };
+                    string[] allowedMods = { "CTG2", "OptimizedCursor", "HighFPSSupport", "TeamSpectate", "ModLoader", "Terraria", "PlayerBars", "Reese", "UnCalamityModMusic"};
 
                     foreach (string modName in clientMods)
                     {
